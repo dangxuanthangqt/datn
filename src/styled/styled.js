5cc28fbf0aa798b2e55b236f2dd48d8159193d74
DXT <dangxuanthangqt@gmail.com>
2021-04-07 23:53:06 +0700

    config stylelint and test CI

---
+++ src/styled/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const BillingInfoFilterWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`
+
+export const BillingInfoFilterWrapper2 = styled.div`
+  display: flex;
+  justify-content: flex-start
+`
